013c3e3ff02eb059146a903f84363812e3fea39d
akubiusa [CinnamonUbuntuCC] <akubiusa@tomacheese.com>
2026-03-02 23:49:40 +0900

    Revert "fix: chezmoi コンフリクト時に自動で skip を選択"
    This reverts commit 558378935754fb8ee0d8b630566ab58e2a960169.

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd "$HOME" || exit
-# コンフリクト発生時に対話プロンプトが出た場合、自動で "s"（skip）を送信する
-yes "s" | sh -c "$(curl -fsSL get.chezmoi.io)" -- update
+sh -c "$(curl -fsSL get.chezmoi.io)" -- update
